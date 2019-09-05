44a0d1fb436182bcccf812b8a517a6023eea1f4e
Ronen Lapushner <ronenk17@gmail.com>
2019-09-05 21:47:08 +0300

    Added skeleton project for the projectinfo service

---
+++ projectinfo/src/main.rs
@@ -0,0 +1,15 @@
+mod handlers;
+
+extern crate warp;
+
+fn handle_about() {
+    unimplemented!()
+}
+
+fn handle_getinfo() {
+    unimplemented!()
+}
+
+fn main() {
+    // Create the routes for the "about" and the "getinfo" commands
+}
