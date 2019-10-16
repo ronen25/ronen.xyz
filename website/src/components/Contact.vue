4c2ab3d7e6ca3c1b2feec26f4eb49e3838d648a5
Ronen Lapushner <ronenk17@gmail.com>
2019-10-16 15:26:49 +0300

    CV/gradesheet buttons will not show if there's no link configured

--- website/src/components/Contact.vue
+++ website/src/components/Contact.vue
@@ -29,28 +29,28 @@
               <b-button
                 variant="danger"
                 :href="cv_pdf_path"
-                v-bind:class="{ disabled: cv_pdf_path == null || cv_pdf_path == '' }"
+                v-if="cv_pdf_path != null && cv_pdf_path != ''"
               >
                 <font-awesome-icon :icon="['fas', 'file-pdf']"></font-awesome-icon>&nbsp;PDF
               </b-button>
               <b-button
                 variant="primary"
                 :href="cv_docx_path"
-                v-bind:class="{ disabled: cv_docx_path == null || cv_docx_path == '' }"
+                v-if="cv_docx_path != null && cv_docx_path != ''"
               >
                 <font-awesome-icon :icon="['fas', 'file-contract']"></font-awesome-icon>&nbsp;DOCX
               </b-button>
               <b-button
                 variant="primary"
                 :href="cv_doc_path"
-                v-bind:class="{ disabled: cv_doc_path == null || cv_doc_path == '' }"
+                v-if="cv_doc_path != null && cv_doc_path != ''"
               >
                 <font-awesome-icon :icon="['fas', 'file-contract']"></font-awesome-icon>&nbsp;DOC
               </b-button>
               <b-button
                 variant="info"
                 :href="cv_odt_path"
-                v-bind:class="{ disabled: cv_odt_path == null || cv_odt_path == '' }"
+                v-if="cv_odt_path != null && cv_odt_path != ''"
               >
                 <font-awesome-icon :icon="['fas', 'file-alt']"></font-awesome-icon>&nbsp;ODT
               </b-button>
@@ -59,16 +59,12 @@
 
           <br />
 
-          <div>
+          <div v-if="gradesheet_pdf_path != null && gradesheet_pdf_path != ''">
             <b>Grade Sheet</b>
 
             <br />
             <!-- TODO: Add link here -->
-            <b-button
-              variant="danger"
-              :href="gradesheet_pdf_path"
-              v-bind:class="{ disabled: gradesheet_pdf_path == null || gradesheet_pdf_path == '' }"
-            >
+            <b-button variant="danger" :href="gradesheet_pdf_path">
               <font-awesome-icon :icon="['fas', 'file-pdf']"></font-awesome-icon>&nbsp;May 2019 Grade Sheet (PDF)
             </b-button>
           </div>
