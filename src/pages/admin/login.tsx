67d3ed3864b34f94b2ebfd2a18e87e435ae0c7d2
Ronen Lapushner <ronenk17@gmail.com>
2022-09-25 22:13:25 +0300

    Fixed login

--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -18,7 +18,7 @@ const Login = () => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession();
+  const session = await getSession(context);
 
   if (session) {
     return {
