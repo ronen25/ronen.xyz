1a8fd80cde12ffc729217164b117edd67c90afaa
Ronen Lapushner <ronenk17@gmail.com>
2023-04-05 22:56:56 +0300

    Added Solid and header

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,31 @@
+import { createSignal } from 'solid-js';
+import SunIcon from '../icons/SunIcon';
+import MoonIcon from '../icons/MoonIcon';
+
+type Theme = 'light' | 'dark';
+
+export default () => {
+  const [theme, setTheme] = createSignal<Theme>('light');
+
+  const prettyThemeName = () => {
+    const themeName = theme();
+    return themeName.charAt(0).toUpperCase() + themeName.slice(1);
+  };
+
+  const onButtonClick = () => {
+    setTheme((prev) => {
+      if (prev === 'dark') return 'light';
+      else return 'dark';
+    });
+  };
+
+  return (
+    <div
+      class='border-2 p-3 cursor-pointer border-gray-400 w-24 flex justify-between transition-all'
+      onClick={onButtonClick}
+    >
+      <span class='font-bold text-gray-400'>{prettyThemeName()}</span>
+      {theme() === 'light' ? <SunIcon /> : <MoonIcon />}
+    </div>
+  );
+};
