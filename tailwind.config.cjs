d15cfa0dc0a0197b894d74ac2f0213b54a8ca72b
Ronen Lapushner <ronenk17@gmail.com>
2023-04-06 14:33:42 +0300

    Design and color fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,21 @@ module.exports = {
     fontFamily: {
       sans: 'Inter, sans-serif',
     },
+    extend: {
+      colors: {
+        'zurich-blue': {
+          400: '#0078bf',
+          600: '#005c93',
+          900: '#00456f',
+        },
+        'zurich-black': '#000000',
+        'zurich-gray': '#919191',
+        'zurich-indigo': '#503a92',
+        'zurich-green': { 400: '#00a030', 900: '#015e1d' },
+        'zurich-amber': { 300: '#f08400', 900: '#9c5701' },
+        'zurich-purple': { 400: '#a30d65', 900: '#6e0a45' },
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
